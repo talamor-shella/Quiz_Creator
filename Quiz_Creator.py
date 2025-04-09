5eebd50971c291e1c9d4cf94d42eea4444264bb4
talamor-shella <shellatalamor@gmail.com>
2025-04-09 18:47:34 +0800

    FileHandling: creates a text file, appends all the questions and answers

--- Quiz_Creator.py
+++ Quiz_Creator.py
@@ -6,32 +6,36 @@ Write the collected data to a text file. Ask another question until the user cho
 
 """
 
-#While loop until invalid
-while True:     
+def quiz_creator():
+    #While loop until invalid
+    while True:     
 
-    #ask user input for a question
-    question = input("Enter a question: ")
+        #ask user input for a question
+        question = input("Enter a question: ")
 
-    #ask for 4 possible answers   
-    a = input("Option a: ")
-    b = input("Option b: ")
-    c = input("Option c: ")
-    d = input("Option d: ")
+        #ask for 4 possible answers   
+        a = input("Option a: ")
+        b = input("Option b: ")
+        c = input("Option c: ")
+        d = input("Option d: ")
     
-    #ask the correct answer
-    correct_answer = input("Enter the correct answer (a/b/c/d): ")
+        #ask the correct answer
+        correct_answer = input("Enter the correct answer (a/b/c/d): ")
 
-    #arranged question and answers
-    print(f"\nQuestion: {question}")
-    print(f"a) {a}")
-    print(f"b) {b}")
-    print(f"c) {c}")
-    print(f"d) {d}")
-    print(f"The correct answer is: {correct_answer}\n")
+        #creates a text file, appends all the questions and answers
+        with open ("quizcreator.txt", "a") as file:
 
-    #ask user if want to add more question
-    add_question = input("Do you want to add more question? (yes or no): ").lower()
+            file.write(f"\nQuestion: {question}")
+            file.write(f"a) {a}\n")
+            file.write(f"b) {b}\n")
+            file.write(f"c) {c}\n")
+            file.write(f"d) {d}\n")
+            file.write(f"The correct answer is: {correct_answer}\n")
 
-    #if statement to check if yes or no
-    if add_question != "yes":
-        break
+        #ask user if want to add more question
+        add_question = input("Do you want to add more question? (yes or no): ").lower()
+
+        #if statement to check if yes or no
+        if add_question != "yes":
+            break
+quiz_creator()
