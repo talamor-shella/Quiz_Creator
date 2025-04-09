547e418cc032be882555a8a27aaf349782269b09
talamor-shella <shellatalamor@gmail.com>
2025-04-09 14:59:19 +0800

    addedpseudocode

--- Quiz_Creator.py
+++ Quiz_Creator.py
@@ -1,2 +1,12 @@
 #Assignment 9- Quiz Creator
 
+"""
+Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. 
+Write the collected data to a text file. Ask another question until the user chose to exit.
+
+"""
+
+#While loop until invalid
+#ask user input for a question
+#ask for 4 possible answers 
+#ask the correct answer
