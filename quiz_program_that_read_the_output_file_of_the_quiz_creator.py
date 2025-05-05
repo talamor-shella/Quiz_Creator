8fefa8ca8207c8049d34337c79457ce1df0b1a5d
talamor-shella <shellatalamor@gmail.com>
2025-05-05 21:10:03 +0800

    FileCreated: quiz_program_that_read_the_output_file_of_the_quiz_creator

---
+++ quiz_program_that_read_the_output_file_of_the_quiz_creator.py
@@ -0,0 +1,8 @@
+#For Assignment 10: Quiz
+
+"""
+Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and 
+check if the answer is correct.
+
+"""
+
